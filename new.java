d2615cc4f679176ffec73ab5e14d08f42c78470e
nicholaswanipaul <nicholaswanipaul@gmail.com>
2018-03-17 15:12:21 +0900

    Create new.java
    dummy data

---
+++ new.java
@@ -0,0 +1,13 @@
+import java.lang.math;
+import Java.lang.Utils;
+public class mydistrict{
+int x, int y;
+public static get_results(x, y)
+{
+x=y-6'
+return y;
+}
+yget_results(4.6);
+
+}
+// this is a dumy function to try github
